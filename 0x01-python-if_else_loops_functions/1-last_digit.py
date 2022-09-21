2b45498c2a63de423d8781ba0923ae01559873fd
idowuseyi <idowuseyi22@gmail.com>
2022-09-20 18:26:54 -0700

    print alphanet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
+last_digit = number % 10
+if number > 0 and last_digit > 5:
+        print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif number > 0 and last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+elif number < 0:
     neg = abs(number)
     lastn_digit = neg % 10
     used_digit = -abs(lastn_digit)
     print(f"Last digit of {number} is {used_digit} and is less than 6 & not 0")
-#elif number > 0:
- #   last_digit = number % 10
-  #  if last_digit > 5:
-   #     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
