271ce9cccaf31c063bbfdbe78d2ee5300059f416
idowuseyi <idowuseyi22@gmail.com>
2022-09-20 21:42:22 -0700

    working jard

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(1, 99):
+    print("{} = {}".format(i, hex(i)))
+    i += 1
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(1, 100):
+    print("{}, ".format(str(i).zfill(2)), end='')
+    i += 1
