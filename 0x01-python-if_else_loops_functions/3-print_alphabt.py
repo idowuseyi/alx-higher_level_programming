779897bbd44115c1d0ab248bb2849fb6d5379644
idowuseyi <idowuseyi22@gmail.com>
2022-09-20 18:08:13 -0700

    print alphanet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == (101 or 113):
+    if i == 101 or i == 113:
         continue
-    print("{}".format(chr(i)), end = '')
+    print("{}".format(chr(i)), end='')
