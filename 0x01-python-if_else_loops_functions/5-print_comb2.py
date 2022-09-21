d55db77ce3623bd0efb5b5657e0238ed4a8c822c
idowuseyi <idowuseyi22@gmail.com>
2022-09-21 11:47:38 -0700

    learning

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 100):
-    print("{}, ".format(str(i).zfill(2)), end='')
+sep = ','
+for i in range(0, 100):
+    if i == (100 - 1):
+        print("{}".format(str(i).zfill(2)), end='')
+    else:
+        print("{}{} ".format(str(i).zfill(2), sep), end='')
     i += 1
+print('\n')
+
+
