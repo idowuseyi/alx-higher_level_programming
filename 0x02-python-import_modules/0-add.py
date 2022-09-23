151ea83757776a9ebe43f32ed401285909f37d63
idowuseyi <idowuseyi22@gmail.com>
2022-09-22 20:45:38 -0700

    calculator

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def add_0():
-    from add_0 import add
+from add_0 import add
+
+if __name__ == '__main__':
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-if __name__ == "__main__":
-    add_0()
+    print("{} + {} = {}".format(a, b, add(a, b))))
