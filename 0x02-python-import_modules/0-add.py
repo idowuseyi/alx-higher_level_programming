3bff357b9b228130c7d1eff55ba8519621613834
idowuseyi <idowuseyi22@gmail.com>
2022-09-22 18:32:38 -0700

    python modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-# importing add_o from its file
-def main():
+def add_0():
     import add_0 as add_num
-    # assigning variable
     a = 1
     b = 2
-    # printing the result
     print(f"{a} + {b} = {add_num.add(1, 2)}")
 if __name__ == "__main__":
-    main()
+    add_0()
