348bf77d2760604ee174a059bac08aa65814b339
idowuseyi <idowuseyi22@gmail.com>
2022-09-22 17:17:59 -0700

    python modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# importing add_o from its file
+import add_0 as add_num
+# assigning variables
+a = 1
+b = 2
+# create a class object
+object = add_num.seyi()
+# printing the result
+print(f"{a} + {b} = {object.add(a, b)}")
