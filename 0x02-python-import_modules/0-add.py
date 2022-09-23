63c0e1e6e369ff1ba9d1296d93353bbbe0601c38
idowuseyi <idowuseyi22@gmail.com>
2022-09-22 18:05:11 -0700

    python modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,6 +6,6 @@ def main():
     a = 1
     b = 2
     # printing the result
-    print(f"{a} + {b} = {add_num.add(a, b)}")
+    print(f"{a} + {b} = {add_num.add(1, 2)}")
 if __name__ == "__main__":
     main()
