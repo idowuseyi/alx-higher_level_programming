d9be6e192cb6cf4d9e76477ba7d93c0527fdd1c8
idowuseyi <idowuseyi22@gmail.com>
2022-09-26 10:46:25 -0700

    learning still

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
+    new_str = [my_string.translate({ord(i): None for i in 'Cc'})]
+    return (" ".join(new_str))
--- 0x03-python-data_structures/5-no_c2.py
+++ 0x03-python-data_structures/5-no_c2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for char in my_string:
-        if char == '\n':
-            break
-        elif char == 'c' or char == 'C':
+        if char == 'c' or char == 'C':
             continue
         else:
-            print("{}".format(char), end="")
+            new_str = ("{}".format(char))
+            return (new_str)
