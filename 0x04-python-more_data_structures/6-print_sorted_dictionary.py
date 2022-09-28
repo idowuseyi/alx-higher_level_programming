d8cced92e74457bd9731b3d3c50e5236fb9ab15d
idowuseyi <idowuseyi22@gmail.com>
2022-09-28 13:39:15 -0700

    updating files

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+def print_sorted_dictionary(a_dictionary):
+
+
+
+    if __name__ == '__name__'
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,20 @@
+def update_dictionary(a_dictionary, key, value):
+
+
+if __name__ == '__main__'
+
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
