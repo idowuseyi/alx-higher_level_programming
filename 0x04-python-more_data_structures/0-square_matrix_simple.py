736e33107d3bc249c67078862e34bbadb7e2c58a
idowuseyi <idowuseyi22@gmail.com>
2022-09-28 13:00:14 -0700

    updating files

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+
+if __name__ == '__main__'
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+               ]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+def uniq_add(my_list=[]):
+
+
+if __name__ == '__main__'
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
