515936dc33bf8731677a752249e37a22659e3089
idowuseyi <idowuseyi22@gmail.com>
2022-09-28 13:05:53 -0700

    updating files

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+def common_elements(set_1, set_2):
+
+
+    if __name__ == '__main__'
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+def only_diff_elements(set_1, set_2):
+
+
+
+if __name__ == '__main__'
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
