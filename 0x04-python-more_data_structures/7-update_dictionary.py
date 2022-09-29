1ef9a83ea993ef052bb0fe4384cc367171cb7185
idowuseyi <idowuseyi22@gmail.com>
2022-09-29 09:40:46 -0700

    more structure operations

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,20 +1,9 @@
+#!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-
-
-if __name__ == '__main__'
-
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+    n = len(a_dictionary)
+    for keys in a_dictionary:
+        if keys == key:
+            a_dictionary[keys] = value
+        else:
+            a_dictionary[key] = value
+        return a_dictionary
