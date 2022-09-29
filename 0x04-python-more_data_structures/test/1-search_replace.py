f15677eefa1c80c2743b21ffea7416ea10e5d6ca
idowuseyi <idowuseyi22@gmail.com>
2022-09-28 22:31:26 -0700

    more data.structure operations

--- 0x04-python-more_data_structures/test/1-search_replace.py
+++ 0x04-python-more_data_structures/test/1-search_replace.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n = len(my_list)
-    for i in range(n):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
-
+    n_list = list(filter(lambda x: ))]
+   ''' n_list = []
+    n_list = my_list
+    n = len(n_list)
+    for i in range(0, n-1):
+        if n_list[i] == search:
+            n_list[i] = replace
+    return n_list
+'''
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
     new_list = search_replace(my_list, 2, 89)
