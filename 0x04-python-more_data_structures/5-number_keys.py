a2c172047ad7ec46931d99eb0567a4fd8c232ec6
idowuseyi <idowuseyi22@gmail.com>
2022-09-28 21:55:54 -0700

    more data.structure operations

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
+#!/usr/bin/python3
 def number_keys(a_dictionary):
-
-
-    if __name__ == '__main__'
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
+    return len(a_dictionary)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    return a_dictionary.items()
 
 
 
-    if __name__ == '__name__'
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+if __name__ == '__name__':
+    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+    print_sorted_dictionary(a_dictionary)
