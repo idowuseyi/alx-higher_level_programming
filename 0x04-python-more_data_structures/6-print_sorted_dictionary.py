758b53d44c0475cac8d6a8163d291354113c9d58
idowuseyi <idowuseyi22@gmail.com>
2022-09-29 07:58:10 -0700

    more structure operations

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for items in sorted(a_dictionary):
-        print("{}: {}".format(items, dico.get(items)))
+    for items in sorted(a_dictionary.items()):
+        print(items)
