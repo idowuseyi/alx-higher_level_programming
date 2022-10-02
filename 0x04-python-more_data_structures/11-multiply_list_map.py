ab324ce093af303da4fe735346edb9b68bb77441
Oluwaseyi Idowu <106804924+idowuseyi@users.noreply.github.com>
2022-10-02 21:53:43 +0100

    multiply list by map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,4 @@
+#!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-
-
-
-
-
-    if __name__ == __main__
-
-    my_list = [1, 2, 3, 4, 6]
-    new_list = multiply_list_map(my_list, 4)
-    print(new_list)
-    print(my_list
+    return list(map(lambda x: x * number, my_list))
 
