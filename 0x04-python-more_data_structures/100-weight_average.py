1103ead4c56fe32eff77d2955631372817b9ca35
idowuseyi <idowuseyi22@gmail.com>
2022-10-02 14:04:45 -0700

    the pythonic way

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -9,9 +9,3 @@ def weight_average(my_list=[]):
     s_xy = sum(map(lambda a: a[0] * a[1], my_list))
     s_x = sum(map(lambda a: a[1], my_list))
     return s_xy / s_x
-
-
-if __name__ == '__main__':
-    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-    result = weight_average(my_list)
-    print("Average: {:0.2f}".format(result))
