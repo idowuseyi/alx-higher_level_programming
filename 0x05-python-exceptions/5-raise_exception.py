0306cdc3757c215f9ec398976991d4adbffad640
idowuseyi <idowuseyi22@gmail.com>
2022-10-10 16:42:06 -0700

    pycodestyle

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -3,7 +3,8 @@ def raise_exception():
     raise TypeError
 
 
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
