a302de2a8362d1ffda5337d5464a0d199843abba
idowuseyi <idowuseyi22@gmail.com>
2022-10-10 16:38:27 -0700

    update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -12,12 +12,12 @@ def safe_print_division(a, b):
 
 
 if __name__ == "__main__":
-a = 12
-b = -1
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+    a = 12
+    b = -1
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
 
-a = 8
-b = -2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+    a = 8
+    b = -2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
