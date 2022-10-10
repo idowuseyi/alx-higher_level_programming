ddd6f121cb9ce6fa32313eb31a0e805af3c2bed5
idowuseyi <idowuseyi22@gmail.com>
2022-10-10 16:43:13 -0700

    pycodestyle

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -6,7 +6,8 @@ def raise_exception_msg(message=""):
         print("{}".format(message), end='')
 
 
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
