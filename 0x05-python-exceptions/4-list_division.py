9a1a21bf934c44ced5e3fbf812c663b0756826ef
idowuseyi <idowuseyi22@gmail.com>
2022-10-10 16:40:31 -0700

    pycodestyle

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,14 +15,14 @@ def list_division(my_list_1, my_list_2, list_length):
 
 
 if __name__ == "__main__":
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1,my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1,my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
 
-print("--")
+    print("--")
 
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1,my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1,my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
