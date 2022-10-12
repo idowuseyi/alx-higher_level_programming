2c6e15bb86d1f1ac9a4b447f229449608a1d4aa4
idowuseyi <idowuseyi22@gmail.com>
2022-10-11 21:46:49 -0700

    class update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python3
-'''
-a class Square that defines a square by: (based on 1-square.py)
-'''
+"""Square class to represent a square"""
 
 
 class Square:
-    __size = None
+    """
+    Defines a Square and its basic properties
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
 
-    def __init__(self, size=0):
-        if type(size) is not int:
+    def __init__(self, size=0) -> None:
+        """
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+        :param size: int size of square ( > 0)
+        """
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
-'''
-Write a class Square that defines a square by: (based on 2-square.py)
-'''
+"""Square class to represent a square"""
 
 
 class Square:
-    __size = None
+    """
+    Defines a Square and its basic properties
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
 
-    def __init__(self, size=0):
-        if type(size) is not int:
+    def __init__(self, size=0) -> None:
+        """
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+        :param size: int size of square ( > 0)
+        """
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
 
-    def area(self):
-        return self.__size * self.__size
+    def area(self) -> int:
+        """
+        Calculates and returns the area of the square
+        :return: the area of the square
+        """
 
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
+        return self.__size ** 2
