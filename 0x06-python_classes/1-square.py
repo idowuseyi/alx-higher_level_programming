1da748a9abb86d4dc56d451fdba0bfdac303c627
idowuseyi <idowuseyi22@gmail.com>
2022-10-11 17:29:48 -0700

    class square

---
+++ 0x06-python_classes/1-square.py
@@ -0,0 +1,23 @@
+'''
+class Square that defines a square by: (based on 0-square.py)
+'''
+
+class Square:
+
+
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
