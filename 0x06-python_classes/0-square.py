a42135bd34c7e7b6a535cebc411b309d5ff0ecd9
idowuseyi <idowuseyi22@gmail.com>
2022-10-11 17:27:10 -0700

    class square

---
+++ 0x06-python_classes/0-square.py
@@ -0,0 +1,13 @@
+'''
+an empty class Square that defines a square:
+'''
+
+
+class Square:
+    pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
