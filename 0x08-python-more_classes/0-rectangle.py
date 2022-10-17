432fc45b8d742f53cc7754a6e24d46d12cfb663b
idowuseyi <idowuseyi22@gmail.com>
2022-10-17 16:53:37 -0700

    python script update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,3 +3,8 @@ class Rectangle:
     """An empty rectangle class"""
     pass
 
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
+
