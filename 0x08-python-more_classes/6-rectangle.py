98ad262c435359502f80a7e26fcc002915c383dd
idowuseyi <idowuseyi22@gmail.com>
2022-10-17 16:13:51 -0700

    test file

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,1 +1,10 @@
 if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    del my_rectangle
+
+    try:
+        print(my_rectangle)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
