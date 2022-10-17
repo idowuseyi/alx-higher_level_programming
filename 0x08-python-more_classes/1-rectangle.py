44e888fb608ae61e6ff00e528e988cc85cb75e0c
idowuseyi <idowuseyi22@gmail.com>
2022-10-17 16:44:17 -0700

    python script update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,31 @@ class Rectangle:
         self.height = height
 
     def width(self, value):
+        if value is None:
+            return 1
+        elif type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.value = value
+
+    def height(self, value):
+        if value is None:
+            return 1
+        elif type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.value = value
+        self.value = value
+
 
 if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
     my_rectangle.width = 10
     my_rectangle.height = 3
     print(my_rectangle.__dict__)
