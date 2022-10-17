4b92e701aae24715e30aec196821f425452a236d
idowuseyi <idowuseyi22@gmail.com>
2022-10-17 16:30:27 -0700

    python script update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def width(self, value):
 
 if __name__ == "__main__":
     my_rectangle.width = 10
