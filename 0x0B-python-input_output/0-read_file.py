d1c636c18b538a7f8cefce6aadefd849669a46c3
idowuseyi <idowuseyi22@gmail.com>
2022-10-25 21:58:47 -0700

    python file handling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,8 @@ def read_file(filename=""):
     """
     This finction read a file
     """
-    with open('filename', encoding='utf-8') as input_file:
-         print(input_file.read())
+    new_file = ""
+    with open("filename") as f:
+        for line in f:
+            new_file = print(line, end="")
+    return new_file
