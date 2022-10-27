12e679a82a7e0aba8f1075c2c60b1da13dedf238
idowuseyi <idowuseyi22@gmail.com>
2022-10-26 22:22:04 -0700

    python file i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,4 @@
 """function that print the content of a utf8 encoded file to stdout"""
 def read_file(filename=""):
     with open(filenane, encoding="utf-8") as f:
-        """ prints the file content """
         print(f.read(), end="")
