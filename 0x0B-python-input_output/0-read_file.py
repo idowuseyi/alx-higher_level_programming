3c06493ae35b38af1ad26af66877ce1b9fc7a6f7
idowuseyi <idowuseyi22@gmail.com>
2022-10-26 21:59:12 -0700

    file io

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@ def read_file(filename=""):
     This finction read a file
     """
     with open('filename') as f:
+        """ print the content of a utf-8 file to a stdout"""
         for line in f:
             print(f.read(), end='')
