96ec0a1bba80e37f40cca13ac3b1fdf8b0cc2083
idowuseyi <idowuseyi22@gmail.com>
2022-12-21 11:50:17 +0100

    javascripting

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/*
+ * This code print a simple text
+ */
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * Write a script that prints 3 lines
+ */
+
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
