222fb38b6478a2423ce428bf551cc79e12ce9d81
idowuseyi <idowuseyi22@gmail.com>
2022-12-21 22:32:42 +0100

    update script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,11 +3,4 @@
  * script that prints two arguments passed to it, in the following format: “ is ”
  */
 const myArgs = process.argv;
-// print process.argv
-let count = 0;
-let i = 0;
-myArgs.forEach((val, index) => {
-  // console.log(`${index}: ${val}`);
-  count += 1;
-});
-console.log(myArgs[2] + ' is ' + myArgs    [3]);
+console.log(myArgs[2] + ' is ' + myArgs[3]);
