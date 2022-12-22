d8a1f1a185632bfd85723e903cba43e421998030
idowuseyi <idowuseyi22@gmail.com>
2022-12-22 03:18:40 +0100

    0 update scripts

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,9 @@
  * script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
  */
 const arg = process.argv;
-console.log(typeof (arg[2]));
-console.log(typeof (int(arg[2])));
+let usedArg = parseInt(arg[2]);
+if (isNaN(usedArg)) {
+	console.log('Not a number');
+} else { 
+	console.log('My number: ', usedArg);
+}
