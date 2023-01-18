cc247896c58a420a5c62ccb78ae66e62bd286a97
idowuseyi <idowuseyi22@gmail.com>
2023-01-17 20:34:57 -0800

    python network #update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode(utf-8)))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
