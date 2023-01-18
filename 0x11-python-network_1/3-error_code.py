711eae6263bc8f8f9e584b19265a4c8ac3d8ed22
idowuseyi <idowuseyi22@gmail.com>
2023-01-17 20:51:06 -0800

    python network #update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 
     try:
         with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode(UTF-8))
+            print(res.read().decode('UTF-8'))
     except error.HTTPError as er:
-        print(Error code:, er.code)
+        print('Error code:', er.code)
