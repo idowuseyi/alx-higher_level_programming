0a8df63fe586abd96149ee7a29820aae7a883605
idowuseyi <idowuseyi22@gmail.com>
2023-01-17 20:31:15 -0800

    python network #update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@
 if __name__ == __main__:
     import urllib.request
 
-    with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as res:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
