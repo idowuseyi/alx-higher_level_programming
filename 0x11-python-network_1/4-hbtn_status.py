e94f6e565ad063a957231a71b760e7c1c791736e
idowuseyi <idowuseyi22@gmail.com>
2023-01-17 20:55:46 -0800

    python network #update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get(https://alx-intranet.hbtn.io/status)
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     t = r.text
-    print(Body response:nt- type: {}nt- content: {}.format(type(t), t))
+    print('Body response:nt- type: {}nt- content: {}'.format(type(t), t))
