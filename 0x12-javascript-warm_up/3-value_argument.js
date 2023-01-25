6903b475f38361bbfd52404487a6c9bfc9aadc43
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2023-01-25 12:12:10 +0000

    javascript update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === undefined ? No argument
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
