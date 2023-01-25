9664f76c521e2815e8681f83dca0e7870077cc28
Oluwaseyi Idowu <idowuseyi22@gmail.com>
2023-01-25 12:10:23 +0000

    javascript update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log(Missing number of occurrences);
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log(C is fun);
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
   }
 }
